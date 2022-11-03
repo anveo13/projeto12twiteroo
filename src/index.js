7e6935313a8edd4e76ce5c067b1262c9d9666d72
Anderson Leonardo <andersonleonardo.sapinto@gmail.com>
2022-11-03 16:25:27 -0300

    Variaveis Globais

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ const tweet = {
   tweet: "eu amo o hub"
 }
 
+const username = {
+	username: 'bobesponja', 
+	avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info" 
+}
+
 app.get("/tweets", (req,res) =>{
     res.send(tweet)
 });
